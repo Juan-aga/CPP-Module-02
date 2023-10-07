3fc61255dc47a2b737599798d720f75c2c1083c6
juan-aga <juan-aga@student.42malaga.com>
2023-10-07 10:53:12 +0200

    ADD: increment/decrement funcions.

--- ex02/inc/Fixed.hpp
+++ ex02/inc/Fixed.hpp
@@ -30,6 +30,9 @@ public:
 	Fixed operator*( Fixed const & rhs ) const;
 	Fixed operator/( Fixed const & rhs ) const;
 
+	Fixed	operator++( void );
+	Fixed	operator++( int );
+
 	bool	operator>( Fixed const & rhs ) const;
 	bool	operator<( Fixed const & rhs ) const;
 	bool	operator>=( Fixed const & rhs ) const;
