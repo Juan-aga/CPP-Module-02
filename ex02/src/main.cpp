ff861a5b56af590a9ed97b4c8e66b870cf28ebbb
juan-aga <juan-aga@student.42malaga.com>
2023-10-07 12:26:32 +0200

    MOD: main like subject with some extra tests.

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -3,46 +3,29 @@
 
 int main( void )
 {
-	Fixed a;
-	Fixed const b( 10 );
-	Fixed const c( 42.42f );
-	Fixed const d( b );
+	Fixed a, c;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
-	a = Fixed( 1234.4321f );
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
 
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "c is " << c << std::endl;
-	std::cout << "d is " << d << std::endl;
+	std::cout << --a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << a << std::endl;
 
-	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
-	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 
-//	return 0;
-	
-/*	Fixed a;
-	Fixed b( a );
-	Fixed c;
+	std::cout << b << std::endl;
+	std::cout << b / c << std::endl;
+	Fixed d (b - b);
+	if (d == c)
+		std::cout << "d and c equal." << std::endl;
 
-	c = b;
-
-*/	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
-	std::cout << d.getRawBits() << std::endl;
-
-	a.setRawBits( 8 );
-//	b = a;
-//	c.setRawBits(42);
-//	d.setRawBits(4222);
-
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
-	std::cout << d.getRawBits() << std::endl;
-
+	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
 
 	return 0;
 }
