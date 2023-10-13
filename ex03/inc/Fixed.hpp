59002e9f9a94d5f3f72d16b2523a558bc277a505
[Juan A.] <juan-aga@student.42.malaga.com>
2023-10-13 10:34:01 +0200

    ADD: files from ex02.

---
+++ ex03/inc/Fixed.hpp
@@ -0,0 +1,52 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+#include <ostream>
+
+class	Fixed
+{
+private:
+
+	int						_raw;
+	static const int		_bits = 8;
+
+public:
+	Fixed( void );
+	Fixed( int num );
+	Fixed( const float num );
+	Fixed( Fixed const & src );
+	~Fixed( void );
+
+	int						getRawBits( void ) const;
+	void					setRawBits( int const raw );
+	float					toFloat( void ) const;
+	int						toInt( void ) const;
+
+	static Fixed &			min( Fixed & f1, Fixed & f2);
+	static const Fixed &	min( Fixed const & f1, Fixed const & f2);
+	static Fixed &			max( Fixed & f1, Fixed & f2);
+	static const Fixed &	max( Fixed const & f1, Fixed const & f2);
+
+	Fixed &					operator=( Fixed const & rhs );
+
+	Fixed					operator+( Fixed const & rhs ) const;
+	Fixed					operator-( Fixed const & rhs ) const;
+	Fixed					operator*( Fixed const & rhs ) const;
+	Fixed					operator/( Fixed const & rhs ) const;
+
+	Fixed					operator++( void );
+	Fixed					operator++( int );
+	Fixed					operator--( void );
+	Fixed					operator--( int );
+
+	bool					operator>( Fixed const & rhs ) const;
+	bool					operator<( Fixed const & rhs ) const;
+	bool					operator>=( Fixed const & rhs ) const;
+	bool					operator<=( Fixed const & rhs ) const;
+	bool					operator==( Fixed const & rhs ) const;
+	bool					operator!=( Fixed const & rhs ) const;
+};
+
+std::ostream &				operator<<( std::ostream & o, Fixed const & i );
+
+#endif
---
+++ ex03/src/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+int main( void )
+{
+	Fixed a, c;
+	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+
+	std::cout << a << std::endl;
+	std::cout << ++a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a++ << std::endl;
+	std::cout << a << std::endl;
+
+	std::cout << --a << std::endl;
+	std::cout << a << std::endl;
+	std::cout << a-- << std::endl;
+	std::cout << a << std::endl;
+
+
+	std::cout << b << std::endl;
+	std::cout << b / c << std::endl;
+	Fixed d (b - b);
+	if (d == c)
+		std::cout << "d and c equal." << std::endl;
+
+	std::cout << Fixed::max( a, b ) << std::endl;
+	std::cout << Fixed::min( a, b ) << std::endl;
+
+	return 0;
+}
